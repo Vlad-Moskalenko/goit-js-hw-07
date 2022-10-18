5954d8f15463526b89aada71415ecc21dd9bd15f
Vlad-Moskalenko <vlad.moskalenko1993@gmail.com>
2022-10-18 21:54:29 +0300

    init: done hw-07

---
+++ src/js/01-gallery.js
@@ -0,0 +1,54 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryWrapper = document.querySelector('.gallery')
+let instance;
+
+window.addEventListener('load', showGallery)
+
+function showGallery() {
+  galleryWrapper.innerHTML = createGallery()
+}
+
+function createGallery(){
+  return (galleryItems.map(({preview, original, description}) => {
+    return (`<div class='gallery__item'><a class='gallery__link' href=${original}><img class='gallery__image' src=${preview}  data-source=${original} alt=${description}/></a></div>`)
+  }).join('')
+  )
+}
+
+galleryWrapper.addEventListener('click', onGalleryImageClick)
+
+function onGalleryImageClick(e){
+  e.preventDefault()
+
+  if(!e.target.classList.contains('gallery__image')) return
+
+  instance = createImageModal(e.target.dataset.source);
+
+  showImageModal(instance)
+}
+
+function createImageModal(url) {
+  return basicLightbox.create(`
+  <div class="modal">
+    <img class="modal__image" src=${url} width="800" height="600">
+  </div>
+`)
+}
+
+function showImageModal(objModal){
+  objModal.show()
+
+  document.querySelector('.modal').addEventListener('click', closeImageModal)
+  window.addEventListener('keydown', closeImageModal)
+}
+
+function closeImageModal(e) {
+  e.target === e.currentTarget || e.code === 'Escape'? instance.close() : null;
+
+  window.removeEventListener('keydown', closeImageModal)
+}
+
+
+
---
+++ src/js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryWrapper = document.querySelector('.gallery')
+
+function showGallery() {
+  galleryWrapper.innerHTML = createGallery()
+}
+
+//як зробити автозапуск функції, при події load бібліотека праює некоректно
+showGallery()
+
+function createGallery(){
+  return (galleryItems.map(({preview, original, description}) => {
+    return (`<a class='gallery__item' href=${original}><img class='gallery__image' src=${preview} alt=${description}/></a>`)
+  }).join('')
+  )
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+ });
+
+
+
+
+
